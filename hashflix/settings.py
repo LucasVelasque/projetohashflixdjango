4e5fc186b06e4ec6d0d97aa8ea35c6b86bec1c13
LucasVelasque <Lucasamaral_velasque@outlook.com>
2023-06-12 13:48:17 -0300

    ajuste para deploy render 2

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -104,14 +104,14 @@ DATABASES = {
     }
 }
 
-import dj_database_url
-
-
-DATABASE_URL = os.getenv('DATABASE_URL')
-if DATABASE_URL:
-    DATABASES = {
-        'defaut': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
-    }
+# import dj_database_url
+#
+#
+# DATABASE_URL = os.getenv('DATABASE_URL')
+# if DATABASE_URL:
+#     DATABASES = {
+#         'defaut': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
