0021b65520b93342a96bad5d02854d5351a422b6
LucasVelasque <Lucasamaral_velasque@outlook.com>
2023-06-12 16:36:15 -0300

    adiconar render.yaml

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -86,19 +87,19 @@ WSGI_APPLICATION = 'hashflix.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "defaut": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
-import dj_database_url
-
-DATABASES['default'] = dj_database_url.config(
-    conn_max_age=600,
-    conn_health_checks=True,
-)
 
 
 # Password validation
