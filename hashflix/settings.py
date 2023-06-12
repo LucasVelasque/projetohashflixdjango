d2e945939a710ef9d117c4fe4c6b0e07ffbb6342
LucasVelasque <Lucasamaral_velasque@outlook.com>
2023-06-12 15:38:33 -0300

    adicionar bd postgress

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -86,36 +86,19 @@ WSGI_APPLICATION = 'hashflix.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-#
-#
-# import dj_database_url
-#
-#
-# DATABASE_URL = os.getenv('DATABASE_URL')
-# if DATABASE_URL:
-#     DATABASES = {
-#         'defaut': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
-#     }
-
-
-import dj_database_url
-
-DATABASES = {}
-DATABASE_URL = os.getenv('DATABASE_URL')
-
-if DATABASE_URL:
-    DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
-else:
-    DATABASES['default'] = {
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600,
+    conn_health_checks=True,
+)
 
 
 # Password validation
